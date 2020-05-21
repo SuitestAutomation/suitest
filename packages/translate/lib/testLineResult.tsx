25395304a1d0108d2e63995036ca83e4906aa1c0
Evgen <evgen.k@suite.st>
2020-05-21 14:27:45 +0300

    SUIT-15213 Check for untranslated messages in translation module

--- packages/translate/lib/testLineResult.tsx
+++ packages/translate/lib/testLineResult.tsx
@@ -23,6 +23,7 @@ import {
 } from '@suitest/types';
 import {translateTestLine} from './testLine';
 import {translateElementProperty} from './condition';
+import {QueryFailedInvalidUrl, OpenAppOverrideFailedError} from '@suitest/types/lib';
 
 const simpleErrorMap: {[key: string]: string} = {
 	failedStart: 'Failed to open application',
@@ -54,17 +55,16 @@ const simpleErrorMap: {[key: string]: string} = {
 	launchExpired: 'Identical scheduling aborted',
 	deviceIsBusy: 'Identical scheduling aborted',
 	notActiveDeveloperMode: 'Failed to launch application. Is "developer mode" turned on? https://suite.st/docs/devices/lg-webos/#enable-developer-mode-on-the-device',
-	// TODO should we display actualValue and expectedValue from response?
 	invalidUrl: 'Application could not be launched. Please verify you have provided URL for this configuration',
 	invalidOpenAppOverrideReference: 'Could not start execution, please check open app override setting. https://suite.st/docs/application/more-configuration-options/#open-app-override-test',
 	Success: 'Command executed successfully',
-	NoSuchElement: 'Element was not found', // TODO params
-	NoSuchFrame: 'Cannot switch to frame', // TODO params
-	UnknownCommand: 'The requested resource could not be found, or a request was received using an HTTP method that is not supported by the mapped resource',  // TODO params
-	StaleElementReference: 'Element is no longer inside DOM', // TODO params
-	ElementNotVisible: 'Element is not currently visible', // TODO params
-	InvalidElementState: 'Cannot perform operation with element because this element is in an invalid state (e.g. attempting to click a disabled element)', // TODO params
-	ElementIsNotSelectable: 'Element is not selectable', // TODO params
+	NoSuchElement: 'Element was not found',
+	NoSuchFrame: 'Cannot switch to frame',
+	UnknownCommand: 'The requested resource could not be found, or a request was received using an HTTP method that is not supported by the mapped resource',
+	StaleElementReference: 'Element is no longer inside DOM',
+	ElementNotVisible: 'Element is not currently visible',
+	InvalidElementState: 'Cannot perform operation with element because this element is in an invalid state (e.g. attempting to click a disabled element)',
+	ElementIsNotSelectable: 'Element is not selectable',
 	UnknownError: 'An unknown server-side error occurred while processing the command',
 	XPathLookupError: 'An error occurred while searching for an element by XPath',
 	Timeout: 'This command takes too long to execute',
@@ -98,6 +98,14 @@ const simpleErrorMap: {[key: string]: string} = {
 	activationExpired: 'Could not open the app because the DevKit/TestKit expired',
 	missingCpp: 'Make sure you have Microsoft Visual C++ Redistributable installed. https://suite.st/docs/devices/playstation',
 	outOfMemory: 'Failed to open the app. Device is out of memory, please restart the device',
+	networkError: 'Network request matching given conditions was not made. https://suite.st/docs/faq/surprises/#network-request-condition-never-matches',
+	instrumentationFailed: 'Suitest was unable to automatically insert the instrumentation library',
+	packageCorrupted: 'Failed to open the app. Please make sure that your app is working correctly',
+	unknownElementProperty: 'Unknown element property',
+	configuratorError: 'Make sure that Apple Configurator 2 and Automation Tools are installed. https://suite.st/docs/devices/apple-tv/#installing-apple-configurator-2',
+	appStoreBuild: 'Can’t install App Store distribution build',
+	outdatedLibraryWarning: 'We have detected that your instrumentation library is outdated, the package can still be opened. Consider updating',
+	cyclicReference: 'Cyclic reference detected',
 };
 
 const translateQueryFailedResults = (result: QueryFailedWithCode): Node => {
@@ -105,38 +113,39 @@ const translateQueryFailedResults = (result: QueryFailedWithCode): Node => {
 		case 'invalidApp':
 			return <text>Wrong app ID detected</text>;
 		case 'applicationError':
-			return <text>Application thrown unexpected error while executing command.</text>;
+			return <text>Application thrown unexpected error while executing command</text>;
 		case 'missingSubject':
-			// TODO: should we display it as general error or implement it for each line when it can happens?
-			return <text>Subject does not exist.</text>;
+			return <text>Subject does not exist</text>;
 		case 'existingSubject':
-			// TODO: should we display it as general error or implement it for each line when it can happens?
-			return <text>Subject expected not to exist.</text>;
+			return <text>Subject expected not to exist</text>;
 		case 'orderErr':
-			return <text>Suitest instrumentation library should be placed as the first script in your HTML file. Loading the instrumentation library dynamically or after other scripts have loaded may cause many unusual errors.</text>;
+			return <text>Suitest instrumentation library should be placed as the first script in your HTML file. Loading the instrumentation library dynamically or after other scripts have loaded may cause many unusual errors</text>;
 		case 'updateAlert':
-			return <text>Suitest instrumentation library is outdated. Please download and install the newest version.</text>;
+			return <text>Suitest instrumentation library is outdated. Please download and install the newest version</text>;
 		case 'wrongExpression':
-			// TODO: what error message should be?
-			// How to reproduce this error?
-			return <text>Wrong expression.</text>;
+			// TODO: How to reproduce this error?
+			return <text>Wrong expression</text>;
 		case 'notFunction':
-			return <text>Specified code is not a function.</text>;
+			return <text>Specified code is not a function</text>;
 		case 'psImplicitVideo':
-			return <text>The "video" subject on the PlayStation platform is inconsistent, we recommend using the "native video" or "element" subject instead.</text>;
+			return <text>The "video" subject on the PlayStation platform is inconsistent, we recommend using the "native video" or "element" subject instead</text>;
 		case 'exprException':
-			return <text>Exception was thrown: "{result.message.info.exception}".</text>;
+			return <text>Exception was thrown: "{result.message.info.exception}"</text>;
+		case 'invalidUrl':
+			const res = result as QueryFailedInvalidUrl;
+
+			return <text>App loaded {res.actualValue} instead of the expected {res.expectedValue}. Consider updating the app URL in settings</text>;
 		default:
 			const _message: never = result.message;
 			console.warn('queryFailed message was not handled', JSON.stringify(_message));
 
 	}
 
-	return <text>Condition was not met.</text>;
+	return <text>Condition was not met</text>;
 };
 
 const translateInvalidInputError = (result: InvalidInputError): TextNode => {
-	const defaultMessage = <text>Test command received invalid input.</text> as TextNode;
+	const defaultMessage = <text>Test command received invalid input</text> as TextNode;
 	const message = 'message' in result ? result.message : undefined;
 
 	if (!message) {
@@ -145,9 +154,9 @@ const translateInvalidInputError = (result: InvalidInputError): TextNode => {
 
 	switch (message.code) {
 		case 'lineTypeNotSupported':
-			return <text>This test command is not supported by the current app configuration.</text> as TextNode;
+			return <text>This test command is not supported by the current app configuration</text> as TextNode;
 		case 'elementNotSupported':
-			return <text>This test command is unsupported by this element.</text> as TextNode;
+			return <text>This test command is unsupported by this element</text> as TextNode;
 		case 'wrongExpression':
 			return defaultMessage;
 		default:
@@ -159,7 +168,7 @@ const translateInvalidInputError = (result: InvalidInputError): TextNode => {
 };
 
 const translateSyntaxError = (result: SyntaxError): TextNode => {
-	const defaultMessage = <text>Test command received invalid input.</text> as TextNode;
+	const defaultMessage = <text>Test command received invalid input</text> as TextNode;
 
 	if (!result.message?.code) {
 		return defaultMessage;
@@ -167,11 +176,11 @@ const translateSyntaxError = (result: SyntaxError): TextNode => {
 
 	switch (result.message.code) {
 		case 'wrongBody':
-			return <text>Body field value is exceeding 4KB limit.</text> as TextNode;
+			return <text>Body field value is exceeding 4KB limit</text> as TextNode;
 		case 'WrongDelay':
-			return <text>Test command received invalid input. Delay value is invalid.</text> as TextNode;
+			return <text>Test command received invalid input. Delay value is invalid</text> as TextNode;
 		case 'wrongUrl':
-			return <text>This does not look like a valid URL.</text> as TextNode;
+			return <text>This does not look like a valid URL</text> as TextNode;
 		default:
 			const _code: never = result.message.code;
 			console.warn('syntaxError message was not handled', JSON.stringify(_code));
@@ -181,7 +190,7 @@ const translateSyntaxError = (result: SyntaxError): TextNode => {
 };
 
 const translateQueryTimeoutError = (result: QueryTimeoutError): TextNode => {
-	const defaultMessage = <text>Application did not respond for 60 seconds.</text> as TextNode;
+	const defaultMessage = <text>Application did not respond for 60 seconds</text> as TextNode;
 
 	if (!result.message) {
 		return defaultMessage;
@@ -191,7 +200,7 @@ const translateQueryTimeoutError = (result: QueryTimeoutError): TextNode => {
 		case 'missingIlResponse':
 			const timeout = result.message.info.timeout / 1000;
 
-			return <text>The wait time exceeded {String(timeout)} {timeout === 1 ? 'second' : 'seconds'}.</text> as TextNode;
+			return <text>The wait time exceeded {String(timeout)} {timeout === 1 ? 'second' : 'seconds'}</text> as TextNode;
 		default:
 			const _code: never = result.message.code;
 			console.warn('queryTimeout message was not handled', JSON.stringify(_code));
@@ -204,10 +213,10 @@ const translateDeviceError = (result: DeviceError): TextNode => {
 	const message = result.message;
 
 	if (message.code === 'unsupportedSelector' && message.info.reason === 'xpathNotSupported') {
-		return <text>Element cannot be found, because this device does not support XPath lookups.</text> as TextNode;
+		return <text>Element cannot be found, because this device does not support XPath lookups</text> as TextNode;
 	}
 	if (message.code === 'deviceFailure' && message.info.reason === 'cssSelectorInvalid') {
-		return <text>CSS selector is invalid.</text> as TextNode;
+		return <text>CSS selector is invalid</text> as TextNode;
 	}
 	if (['videoAdapterInvalidOutput', 'videoAdapterNotFunction', 'videoAdapterThrownError'].includes(message.code)) {
 		return <text>Video adapter error: {message.info.reason}</text> as TextNode;
@@ -223,35 +232,35 @@ const translateUnsupportedButtonError = (result: UnsupportedButtonError): TextNo
 		return <text>Specified buttons are not supported on this device: [{buttonIds.join(', ')}]</text> as TextNode;
 	}
 
-	return <text>Specified buttons are not supported on this device.</text> as TextNode;
+	return <text>Specified buttons are not supported on this device</text> as TextNode;
 };
 
 const translateAbortedError = (result: AbortedError): TextNode => {
 	if (result.message?.info.reason === 'manualActionRequired') {
-		return <text>Manual actions are not supported.</text> as TextNode;
+		return <text>Manual actions are not supported</text> as TextNode;
 	}
 
-	return <text>Test execution was aborted.</text> as TextNode;
+	return <text>Test execution was aborted</text> as TextNode;
 };
 
 const translateInvalidVariableError = (result: InvalidVariableError): TextNode => {
 	const vars = result.args?.variables ?? [];
 
 	if (vars.length === 0) {
-		return <text>Variable is not defined in the app configuration.</text> as TextNode;
+		return <text>Variable is not defined in the app configuration</text> as TextNode;
 	}
 
 	const textVars = vars.join(', ');
 
 	return <text>{
 		vars.length === 1 ? `Variable ${textVars} is` : `Variables ${textVars} are`
-	} not defined in the app configuration.</text> as TextNode;
+	} not defined in the app configuration</text> as TextNode;
 };
 
 const translateInvalidValueError = (result: InvalidValueError): TextNode =>
 	result.args?.propertyName ?
-		<text>Invalid value provided for {result.args.propertyName}.</text> as TextNode :
-		<text>Invalid value provided.</text> as TextNode;
+		<text>Invalid value provided for {result.args.propertyName}</text> as TextNode :
+		<text>Invalid value provided</text> as TextNode;
 
 const translateInvalidResultError = (result: InvalidResultError): TextNode => {
 	const defaultMessage = <text>Unexpected response received</text> as TextNode;
@@ -262,7 +271,7 @@ const translateInvalidResultError = (result: InvalidResultError): TextNode => {
 
 	switch (result.message.code) {
 		case 'resultTooLong':
-			return <text>Response exceeded the size limit of 4KB.</text> as TextNode;
+			return <text>Response exceeded the size limit of 4KB</text> as TextNode;
 		default:
 			const _code: never = result.message.code;
 			console.warn('invalidResult code was not handled', JSON.stringify(_code));
@@ -271,9 +280,12 @@ const translateInvalidResultError = (result: InvalidResultError): TextNode => {
 	return defaultMessage;
 };
 
-// TODO: should we somehow use additional data from result?
-const translateOpenAppOverrideFailedError = (): TextNode =>
-	<text>An error occurred while executing the "Open app override test".</text> as TextNode;
+// TODO: investigate possible additional errorType's, and "message" is completely valid result.
+const translateOpenAppOverrideFailedError = (result: OpenAppOverrideFailedError): Node =>
+	'lineId' in result.message ?
+		<text>Open app override failed on line {result.message.lineId}</text> as TextNode :
+		<text>Open app override failed</text>;
+
 
 const translateInvalidReferenceError = (result: InvalidReferenceError): TextNode => {
 	const start = 'Could not start execution because';
@@ -285,20 +297,20 @@ const translateInvalidReferenceError = (result: InvalidReferenceError): TextNode
 
 const translateADBError = (result: ADBError): TextNode => <text>{
 		result.message?.info.reason ??
-		'ADB communication with the device has failed. Make sure your device is set up correctly and it can be connected to using ADB.'
+		'ADB communication with the device has failed. Make sure your device is set up correctly and it can be connected to using ADB'
 	}</text> as TextNode;
 
 const translateInvalidPackageError = (result: InvalidPackageError): TextNode => {
-	const defaultMessage = <text>Package cannot be launched on simulator device.</text> as TextNode;
+	const defaultMessage = <text>Package cannot be launched on simulator device</text> as TextNode;
 
 	if (!result.message?.code) {
 		return defaultMessage;
 	}
 	switch (result.message.code) {
 		case 'appleTvDevicePackageOnSim':
-			return <text>An Apple TV app package cannot be launched on simulator device.</text> as TextNode;
+			return <text>An Apple TV app package cannot be launched on simulator device</text> as TextNode;
 		case 'appleTvSimPackageOnDevice':
-			return <text>An Apple TV app simulator package cannot be launched on real device.</text> as TextNode;
+			return <text>An Apple TV app simulator package cannot be launched on real device</text> as TextNode;
 		default:
 			const _code: never = result.message.code;
 			console.warn('invalidPackage code was not handled', JSON.stringify(_code));
@@ -313,11 +325,11 @@ const translateOutdatedLibraryError = (result: OutdatedInstrumentationLibraryErr
 	switch (result.message.code) {
 		case 'major':
 			return <text>
-				We have detected that your instrumentation library is outdated and the package cannot be opened. Update required.
+				We have detected that your instrumentation library is outdated and the package cannot be opened. Update required
 			</text> as TextNode;
 		case 'minor':
 			return  <text>
-				We have detected that your instrumentation library is outdated, the package can still be opened. Consider updating.
+				We have detected that your instrumentation library is outdated, the package can still be opened. Consider updating
 			</text> as TextNode;
 	}
 
@@ -346,10 +358,6 @@ const translateInvalidRepositoryReference = (result: InvalidRepositoryReferenceE
 	return <text>{textMsg}</text> as TextNode;
 };
 
-// TODO: investigate how to get jsExpressionError, testSnippetError, outdatedLibraryWarning described in suitest-js-api
-// TODO: investigate handlers from FE:
-//  	cyclicReference, snippetError, snippetUndone, instrumentationFailed, packageCorrupted, unknownElementProperty
-//  	configuratorError, appStoreBuild
 // TODO: better test coverage
 export const translateResultMessage = (result: TestLineErrorResult): Node => {
 	if (result.errorType in simpleErrorMap) {
@@ -359,7 +367,7 @@ export const translateResultMessage = (result: TestLineErrorResult): Node => {
 		return translateQueryFailedResults(result);
 	}
 	if (result.errorType === 'queryFailed') {
-		return <text>Condition was not met.</text>;
+		return <text>Condition was not met</text>;
 	}
 	if (result.errorType === 'invalidInput') {
 		return translateInvalidInputError(result);
@@ -389,7 +397,7 @@ export const translateResultMessage = (result: TestLineErrorResult): Node => {
 		return translateInvalidResultError(result);
 	}
 	if (result.errorType === 'openAppOverrideFailed') {
-		return translateOpenAppOverrideFailedError();
+		return translateOpenAppOverrideFailedError(result);
 	}
 	if (result.errorType === 'invalidReference') {
 		return translateInvalidReferenceError(result);
@@ -409,7 +417,7 @@ export const translateResultMessage = (result: TestLineErrorResult): Node => {
 
 	console.warn('Error message not implemented for', JSON.stringify(result));
 
-	return <text>Unknown error occurred.</text>;
+	return <text>Unknown error occurred</text>;
 };
 
 export const translateTestLineResult = (options: {
