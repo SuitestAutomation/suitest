89496529ea8df4c6ee9560d143c7587bb18907d5
JeKachan <jekachan7@gmail.com>
2025-07-25 15:10:12 +0300

    SUIT-21880 Added translation for vidaaPlatformError not started reason

--- packages/translate/lib/notStartedReason.tsx
+++ packages/translate/lib/notStartedReason.tsx
@@ -161,6 +161,11 @@ export function translateNotStartedReason(code: NotStartedReason): Translation {
 			return {
 				title: <text>Your Comcast Xfinity key has been expired.</text>,
 			};
+		case NOT_STARTED_REASON.VIDAA_PLATFORM_ERROR:
+			return {
+				title: <text>Cannot continue: Platform failed</text>,
+				description: <text>Please verify that the device is online and it's current IP address is correctly specified in Suitest. If nothing helps try rebooting the device.</text>,
+			};
 		default:
 			const _code: never = code;
 			throw new Error(`Unknown not started reason received: ${_code}`);
